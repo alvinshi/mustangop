6b0830dceeea06e842e956ffa69f8effcff0fde2
wujiangwei <wujiangwei55@gmail.com>
2016-05-05 20:57:19 +0800

    angular JS Succeed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var domain = require('domain');
 var express = require('express');
 var path = require('path');
+var ejs = require('ejs');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var todos = require('./routes/todos');
@@ -11,7 +12,8 @@ var app = express();
 
 // 设置 view 引擎
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html');
 app.use(express.static('public'));
 
 // 加载云代码方法
@@ -54,42 +56,10 @@ app.use(function (req, res, next) {
     next();
 });
 
-var router = express.Router();
-
-// 没有挂载路径的中间件，通过该路由的每个请求都会执行该中间件
-router.use(function (req, res, next) {
-    console.log('Router Time:', Date.now());
-    next();
+// angular 测试
+app.get('/test/angular', function(req, res) {
+    res.render('testAngular');
 });
-
-// 一个中间件栈，显示任何指向 /user/:id 的 HTTP 请求的信息
-router.use('/user/:id', function(req, res, next) {
-    console.log('Request URL:', req.originalUrl);
-    next();
-}, function (req, res, next) {
-    console.log('Request Type:', req.method);
-    next();
-});
-
-// 一个中间件栈，处理指向 /user/:id 的 GET 请求
-router.get('/user/:id', function (req, res, next) {
-    // 如果 user id 为 0, 跳到下一个路由
-    if (req.params.id == 0) next('route');
-    // 负责将控制权交给栈中下一个中间件
-    else next(); //
-}, function (req, res, next) {
-    // 渲染常规页面
-    res.end('regular');
-});
-
-// 处理 /user/:id， 渲染一个特殊页面
-router.get('/user/:id', function (req, res, next) {
-    console.log(req.params.id);
-    res.end('special');
-});
-
-// 将路由挂载至应用
-app.use('/me/', router);
 
 //end test
 
