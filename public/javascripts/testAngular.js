0367737a4bbdcc142e1890940f39309f5476e0b1
wujiangwei <wujiangwei55@gmail.com>
2016-05-07 11:40:58 +0800

    navigation

--- public/javascripts/testAngular.js
+++ public/javascripts/testAngular.js
@@ -1,10 +1,27 @@
 /**
  * Created by wujiangwei on 16/5/5.
  */
-angular.module('myApp', []).controller('namesCtrl', function($scope) {
+var app=angular.module('myApp',[]);
+
+app.controller('namesCtrl', function($scope) {
     $scope.names = [
-        {name:'Jani',country:'Norway'},
+        {name:'Jani',country:'Test'},
         {name:'Hege',country:'Sweden'},
         {name:'Kai',country:'Denmark'}
     ];
+    $scope.name = 'First ';
 });
+
+app.directive('exampleDirective', function() {
+    return {
+        restrict: 'E',
+        template: '<p>Hello {{name}}!</p>',
+        controller: function($scope, $element){
+            $scope.name = $scope.name + "Second ";
+        },
+        link: function(scope, el, attr) {
+            scope.name = scope.name + "Third ";
+        }
+    }
+})
+
