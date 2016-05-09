e33a8231c22bb20b9bd9560c84368c81d626d1fa
wujiangwei <wujiangwei@wudeMacBook-Pro.local>
2016-05-09 20:35:01 +0800

    iTunes Search debug

---
+++ public/javascripts/itunesApi.js
@@ -0,0 +1,43 @@
+/**
+ * Created by wujiangwei on 16/5/9.
+ */
+
+
+var app=angular.module('itunesSearch',[]);
+
+app.controller('itunesSearchControl', function($scope, $http) {
+
+    $scope.searchKey = '';
+
+    $scope.searchApp = function(){
+        if ($scope.searchUrl != ''){
+
+            var searchUrl = 'api/itunes/search/' + $scope.searchKey;
+
+            $http.get(searchUrl).success(function(response){
+                console.log('angular search end');
+                console.log(response);
+                $scope.appResults = response.appResults;
+                $scope.errorMsg = 'wwwwww沙盒';
+
+                $scope.testResults = ['test0', 'test1', 'test2'];
+            });
+        }
+    };
+
+
+
+});
+
+//app.directive('itunesSearchDirective', function() {
+//    return {
+//        restrict: 'AE',
+//        template: '<p>Hello {{name}}!</p>',
+//        controller: function($scope, $element){
+//            $scope.name = $scope.name + "Second ";
+//        },
+//        link: function(scope, el, attr) {
+//            scope.name = scope.name + "Third ";
+//        }
+//    }
+//})
--- routes/api.js
+++ routes/api.js
@@ -5,16 +5,68 @@
 var express = require('express');
 var router = express.Router();
 var AV = require('leanengine');
+var https = require('https');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.send('api home')
-    //res.sendFile('./views/index.html');
+    res.send('api home support by wujiangwei')
 });
 
 
+//iTunes Store Api
+router.get('/itunes/search/:searchkey', function(req, res, next) {
+
+    //https://itunes.apple.com/search?term=美丽约&country=cn&entity=software
+    var searchUrl = 'https://itunes.apple.com/search?term=' + req.params.searchkey +'&country=cn&entity=software'
 
 
+    https.get(searchUrl, function(httpRes) {
+
+        console.log('statusCode: ', httpRes.statusCode);
+        console.log('headers: ', httpRes.headers);
+        var totalLen = 0;
+        var totalData = ''
+
+        if (httpRes.statusCode != 200){
+            console.log("Got error: " + httpRes.statusMessage);
+            res.json({'appResults':[], 'errorMsg' : httpRes.statusCode + httpRes.statusMessage})
+        }else {
+            httpRes.on('data', function(data) {
+                totalData += data;
+            });
+
+            httpRes.on('end', function(){
+                var dataStr = totalData.toString()
+
+                var dataObject = eval("(" + dataStr + ")")
+
+                var appResults = Array();
+
+                for (var i = 0; i < dataObject.results.length; i++){
+                    var appInfo = dataObject.results[i];
+                    var appResult = Object();
+                    appResult.name = appInfo['trackCensoredName'];
+                    appResult.icon = appInfo['artworkUrl100'];
+                    appResult.appid = appInfo['artistId'];
+                    appResult.lastReleaseDate = appInfo['currentVersionReleaseDate'];
+                    appResult.seller = appInfo['sellerName'];
+
+                    //类别 平台信息
+
+                    appResults.push(appResult);
+                }
+
+                console.log(appResults);
+                res.json({'appResults':appResults, 'title':'Api Todo'});
+            })
+        }
+
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+        res.json({'appResults':[], 'errorMsg' : e.message})
+    });
+});
 
 //test code
 router.get('/test/todos', function(req, res, next) {
@@ -25,18 +77,21 @@ router.get('/test/todos', function(req, res, next) {
     query.descending('createdAt');
     query.find({
         success: function(results) {
-            var todos = new Array()
+            var todos = new Array();
             for (var i = 0; i < results.length; i++)
             {
-                todos[i] = results[i].get('content')
+                var todo = Object()
+                todo.content = results[i].get('content');
+                todos[i] = todo
+                //todos[i] = results[i].get('content');
             }
 
-            res.json({'todos':todos, 'title':'Api Todo'})
+            res.json({'todos':todos, 'title':'Api Todo'});
         },
         error: function(err) {
             if (err.code === 101) {
 
-                res.json({'title':'101 Error'})
+                res.json({'title':'101 Error'});
                 res.render('todos', {
                     title: 'TODO 列表',
                     todos: []
