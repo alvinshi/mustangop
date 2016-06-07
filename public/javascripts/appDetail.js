9dc70401ef12ef042647766079426baac6b8a1af
才龙 <hengxing0052@163.com>
2016-06-07 13:41:11 +0800

    appDetail limit

--- public/javascripts/appDetail.js
+++ public/javascripts/appDetail.js
@@ -18,9 +18,62 @@ app.controller('myAppControl', function($scope, $http, $location, FileUploader) 
         });
     });
 
+    //删除当前交换记录
+    $scope.releaseBtnClick = function(appid,appversion){
+        $scope.prepareReleaseAppid = appid;
+        $scope.prepareReleaseVersion = appversion;
+    };
+
+    $scope.removeHistory = function(){
+        var releaseHistoryUrl = '/myapp/history/delete';
+
+        var myAppId = $scope.appBaseInfo.appleId;
+        var myAppVersion = $scope.appBaseInfo.version;
+
+        var postParam = {'myAppId' : myAppId, 'myAppVersion' : myAppVersion,
+            'hisAppId' : $scope.prepareReleaseAppid, 'hisAppVersion' : $scope.prepareReleaseVersion};
+        console.log('add history' + postParam);
+
+        $http.post(releaseHistoryUrl, postParam).success(function(response){
+            if (response.errorId == 0){
+                console.log('remove app if');
+
+                if ($scope.appResults != undefined){
+                    //change ui
+                    for (var q = 0; q < $scope.appResults.length; q++){
+                        var appRe = $scope.appResults[q];
+
+                        if (appRe.appleId === $scope.prepareReleaseAppid){
+                            appRe.isExced = false;
+                            console.log(appRe.appleId + 'is exchanged');
+                            break;
+                        }
+                    }
+                }
+
+                //other thing
+                for (var i = 0; i < $scope.myExcAllApps.length; i++){
+                    var app = $scope.myExcAllApps[i];
+                    if (app.appleId == $scope.prepareReleaseAppid){
+                        console.log('remove app to ui');
+                        $scope.myExcAllApps.splice(i, 1);
+                        break;
+                    }
+                }
+
+                $scope.errorMsg = '';
+            }else {
+                console.log('remove app else');
+                $scope.errorMsg = response.errorMsg;
+            }
+
+        });
+    };
+
     //搜索iTunes
     $scope.searchHistoryApp = function () {
         $scope.isError = 0;
+        $scope.progressNum = 100;
 
         if ($scope.searchUrl != '') {
 
@@ -28,8 +81,34 @@ app.controller('myAppControl', function($scope, $http, $location, FileUploader) 
 
             $http.get(searchUrl).success(function (response) {
 
-                $scope.appResults = response.appResults;
+                $scope.pageSize = 6; //每页显示条数
+                $scope.pagedItems = [];
+                $scope.currentPage = 0; // 当前页
 
+                var totalCount = response.appResults; //获取总数list
+
+                // 处理每页获取的数据逻辑
+                for (var e = 0; e < totalCount.length; e++) {
+                    if (e % $scope.pageSize === 0) {
+                        $scope.pagedItems[Math.floor(e / $scope.pageSize)] = [ totalCount[e] ];
+                    } else {
+                        $scope.pagedItems[Math.floor(e / $scope.pageSize)].push(totalCount[e]);
+                    }
+                }
+
+                // 上一页
+                $scope.prevPage = function () {
+                    if ($scope.currentPage > 0) {
+                        $scope.currentPage--;
+                    }
+                };
+
+                // 下一页
+                $scope.nextPage = function () {
+                    if ($scope.currentPage < $scope.pagedItems.length - 1) {
+                        $scope.currentPage++;
+                    }
+                };
 
                 $scope.progressNum = 0;
 
@@ -38,19 +117,28 @@ app.controller('myAppControl', function($scope, $http, $location, FileUploader) 
                     $scope.errorMsg = response.errorMsg;
                 } else {
                     $scope.errorMsg = '';
-                    if ($scope.appResults.length == 0) {
+                    if (totalCount.length == 0) {
                         $scope.isError = 1;
                         $scope.errorMsg = '未找到你搜索的App';
                     }
                 }
 
                 //console.log($scope.appResults);
-            });
+            })
+        }
+    };
+
+    $scope.keySearchApp = function(e){
+        var keycode = window.event?e.keyCode:e.which;
+        //console.log('keycode ' + keycode);
+        //enter or space
+        if(keycode==13 || keycode==32){
+            $scope.searchHistoryApp();
         }
     };
 
 
-    // 添加交换的应用
+    // 弹框里面添加交换的应用
     $scope.addHistory = function(hisAppInfo){
 
         var addHistoryUrl = '/myapp/addHistory/' + appid + '/' + $scope.appBaseInfo.version;
