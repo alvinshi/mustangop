db9914f9b235b7bc8cdc03a34aecbccde34fb8de
wujiangwei55 <wujiangwei55@gmail.com>
2016-06-13 16:07:11 +0800

    refine 1

--- public/javascripts/taskDetailMob.js
+++ public/javascripts/taskDetailMob.js
@@ -10,5 +10,16 @@ app.controller('taskDetailMobControl', function($scope, $http, $location){
     var detailUrl = '/taskDetail/detail' + '/' + appleId;
     $http.get(detailUrl).success(function(response){
         $scope.oneAppInfo = response.oneAppInfo;
-    })
+    });
+
+    $scope.normalBtnShow = 0;
+    if(getCookie('uploadImgName').length == 0){
+        normalBtnShow = 0;
+    }else {
+        normalBtnShow = 1;
+    }
+
+    console.log('start' + getCookie('uploadImgName'));
+    setCookie('uploadImgName', 'wujiangwei', 365);
+    console.log('end' + getCookie('uploadImgName'));
 });
