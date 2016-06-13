f1a3676ff70d1ffc6766dcf1e8a44ec9e4de4fe4
tanghui <tanghui@yangyangdeAir.lan>
2016-06-13 16:29:36 +0800

    appdetail

--- public/javascripts/taskDetailMob.js
+++ public/javascripts/taskDetailMob.js
@@ -3,33 +3,23 @@
  */
 
 var app = angular.module('taskDetailMobContent', []);
-app.controller('taskDetailMobControl', function($scope, $http, $location){
+app.controller('taskDetailMobControl', function($scope, $http, $location) {
     var appurlList = $location.absUrl().split('/');
     var appleId = appurlList[appurlList.length - 1];
 
     var detailUrl = '/taskDetail/detail' + '/' + appleId;
-    $http.get(detailUrl).success(function(response){
+    $http.get(detailUrl).success(function (response) {
         $scope.oneAppInfo = response.oneAppInfo;
     });
 
-
-    
     $scope.normalBtnShow = 0;
-    if(getCookie('uploadImgName').length == 0){
+    if (getCookie('uploadImgName').length == 0) {
         normalBtnShow = 0;
-    }else {
+    } else {
         normalBtnShow = 1;
     }
 
-    console.log('start' + getCookie('uploadImgName'));
-    setCookie('uploadImgName', 'wujiangwei', 365);
-    console.log('end' + getCookie('uploadImgName'))
-    
 });
-
-
-
-
 
 
 
