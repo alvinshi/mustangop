9f841d9488427f59bf2022b28a9bfc55a9aa7f07
才龙 <hengxing0052@163.com>
2016-06-14 11:42:53 +0800

    task

--- public/javascripts/taskDetailMob.js
+++ public/javascripts/taskDetailMob.js
@@ -12,13 +12,34 @@ app.controller('taskDetailMobControl', function($scope, $http, $location, FileUp
         $scope.oneAppInfo = response.oneAppInfo;
     });
 
-    var prepareSaveApp = undefined;
+    $scope.Confirm = function(){
+        var userUrl = '/taskDetailMobile/add/' + appleId;
+        $http.post(userUrl,{'extUserName':$scope.extUserName}).success(function(response){
+            $scope.errorId = response.errorId;
+            $scope.errorMsg = response.errorMsg;
+
+        })
+    };
+
 
     //upload file
     var uploader = $scope.uploader = new FileUploader({
         url: '/upload/img',
         queueLimit: 3
     });
+
+    $scope.saveImage = function () {
+
+        //一次只能保存一个任务,不支持多个同时保存
+        if (!uploader.isUploading) {
+            $scope.saveMgs = '';
+
+            //上传成功的回掉里,保存换评参数
+        } else {
+            $scope.saveMgs = '请等待上个任务保存成功';
+        }
+
+    };
 
     uploader.filters.push({
         name: 'imageFilter',
@@ -32,12 +53,7 @@ app.controller('taskDetailMobControl', function($scope, $http, $location, FileUp
         $scope.files = addedFileItems;
         console.info('onAfterAddingAll', addedFileItems);
     };
-    uploader.onBeforeUploadItem = function (item) {
-        console.info('onBeforeUploadItem', item);
-    };
-    uploader.onProgressItem = function (fileItem, progress) {
-        console.info('onProgressItem', fileItem, progress);
-    };
+
     uploader.onProgressAll = function (progress) {
         console.info('onProgressAll', progress);
     };
@@ -53,11 +69,10 @@ app.controller('taskDetailMobControl', function($scope, $http, $location, FileUp
     uploader.onCompleteItem = function (fileItem, response, status, headers) {
         console.info('onCompleteItem', fileItem, response, status, headers);
 
-        var appUrl = 'addTask/' + prepareSaveApp.appExcTaskObjectID;
+        var appUrl = '/taskDetailMobile/addTask/' + appleId;
 
         $http.post(appUrl, {
-                'extUserName':response.extUserName,
-                'requirementImg': response.fileUrlList
+                'requirementImg': response.fileUrlList[0]
             })
             .success(function (response) {
                 $scope.errorId = response.errorId;
@@ -67,6 +82,8 @@ app.controller('taskDetailMobControl', function($scope, $http, $location, FileUp
     uploader.onCompleteAll = function () {
         console.info('onCompleteAll');
     };
+
+    console.info('uploader', uploader);
 
     $scope.normalBtnShow = 0;
     if (getCookie('uploadImgName').length == 0) {
--- routes/taskDetailMobApi.js
+++ routes/taskDetailMobApi.js
@@ -15,18 +15,48 @@ router.get('/:appleId', function(req, res) {
     res.render('taskDetailMobile')
 });
 
-function addTaskDetail(res, appExcObject, hisAppId, hisAppInfo){
+router.post('/add/:appleId', function(req, res){
+    var appleid = req.params.appleId;
+    var extUserName = req.body.extUserName;
     var myDate = new Date();
     var myDateStr = myDate.getFullYear() + '-' + (parseInt(myDate.getMonth())+1) + '-' + myDate.getDate() + ' ' +
         myDate.getHours() + ':' + myDate.getMinutes() + ':' + myDate.getSeconds();     //获取当前日期
 
-
-
-}
+    var query = new AV.Query(mackTaskInfo);
+    query.startsWith('excDateStr', myDateStr);
+    query.exists('extUserName');
+    query.find().then(function(results){
+        if (results.length < 1){
+            var mackTaskObject = new mackTaskInfo();
+            mackTaskObject.set('extUserName', extUserName);
+            mackTaskObject.set('excDateStr', myDateStr);
+            mackTaskObject.save().then(function(){
+                //成功
+            })
+        }
+    });
+});
 
 // 新增 做任务详情
 router.post('/addTask/:appleId', function(req, res){
-    var hisAppId = req.params.appleId;
+    var requirementImg = req.body.requirementImg;
+
+    var myDate = new Date();
+    var myDateStr = myDate.getFullYear() + '-' + (parseInt(myDate.getMonth())+1) + '-' + myDate.getDate() + ' ' +
+        myDate.getHours() + ':' + myDate.getMinutes() + ':' + myDate.getSeconds();     //获取当前日期
+
+    var query = new AV.Query(mackTaskInfo);
+    query.startsWith('excDateStr', myDateStr);
+    query.exists('requirementImg');
+    query.find().then(function(results){
+        if (results.length < 1){
+            var mackTaskObject = new mackTaskInfo();
+            mackTaskObject.set('requirementImg', [requirementImg]);
+            mackTaskObject.save().then(function(){
+                //成功
+            })
+        }
+    })
 
 });
 
