ed098f15fd056c1492eb9cafb66c7ed873aa650a
才龙 <hengxing0052@163.com>
2016-06-28 15:47:03 +0800

    exchistory fix

--- cloud.js
+++ cloud.js
@@ -16,25 +16,6 @@ AV.Cloud.define('hello', function(request, response) {
 AV.Cloud.define('refreshTask', function(request, response) {
     //1.每天早上8点,把今天之前完成的任务,都设置为已经完成
     //2.每天早上8点,把1周之前完成的任务,相关的信息删掉,只单纯保留任务信息
-    var myDate = new Date();
-    var myDateStr = myDate.getFullYear() + '-' + (parseInt(myDate.getMonth())+1) + '-' + myDate.getDate();
-
-    var query = new AV.Query(IOSAppExcLogger);
-    query.notContainedIn('excDateStr', myDateStr);
-    query.exists('requirementImg');
-    query.exists('totalExcCount');
-    query.exists('excKinds');
-    query.find().then(function(result){
-        for (var i = 0; i < result.length; i++){
-            var totalCount = result[i].get('totalExcCount');
-            var addTaskCount = result[i].get('taskCount');
-            var SurplusCount = totalCount - addTaskCount;
-            if (SurplusCount == 0){
-                result.splice(SurplusCount)
-            }
-        }
-    });
-
     console.log('Log in timer.');
     response.success('refreshTask');
 });
