c3c7a9536ba67cd94651d8c1ccef7c5345f94f94
才龙 <hengxing0052@163.com>
2016-06-28 23:26:56 +0800

    Fix timer bug

--- cloud.js
+++ cloud.js
@@ -29,30 +29,55 @@ function getRefreshTaskQuery(){
 AV.Cloud.define('refreshTask', function(request, response) {
 
     var query = getRefreshTaskQuery();
-    var totalCount = query.count();
-    var remain = totalCount % 1000;
 
-    var totalForCount = totalCount/1000 + remain > 0 ? 1 : 0;
+    query.count().then(function (count) {
+        var totalCount = count;
 
-    for (var i = 0; i < totalForCount; i++){
-        var subQuery = getRefreshTaskQuery();
-        subQuery.limit(1000);
-        subQuery.skip(i * 1000);
-        query.find().then(function(results){
-            for (var i = 0; i < results.length; i++){
-                results[i].set('taskStatus', 1);
-            }
-            AV.Object.saveAll(results).then(function (avobjs) {
-                //if (i == totalForCount - 1)
-                {
-                    console.log('!!!!! refreshTask succeed');
-                    response.success('refreshTask');
+        if (totalCount == 0){
+            console.log('!!!!! refreshTask succeed: no task need to deal');
+            response.success('refreshTask');
+            return;
+        }
+
+        var remain = totalCount % 1000;
+        var totalForCount = totalCount/1000 + remain > 0 ? 1 : 0;
+
+        for (var i = 0; i < totalForCount; i++){
+            var subQuery = getRefreshTaskQuery();
+            subQuery.limit(1000);
+            subQuery.skip(i * 1000);
+            query.find().then(function(results){
+                for (var i = 0; i < results.length; i++){
+                    results[i].set('taskStatus', 1);
                 }
-            }, function (error) {
-                console.log('----- refreshTask error');
+                AV.Object.saveAll(results).then(function (avobjs) {
+                    //if (i == totalForCount - 1)
+                    {
+                        console.log('!!!!! refreshTask succeed');
+                        response.success('refreshTask');
+                    }
+                }, function (error) {
+                    console.log('----- refreshTask error');
+                });
             });
-        });
-    }
+        }
+    }, function (error) {
+        console.log('----- refreshTask error: count error');
+    });
+
 });
 
 module.exports = AV.Cloud;
+
+
+var paramsJson = {
+    movie: "夏洛特烦恼"
+};
+AV.Cloud.run('refreshTask', paramsJson, {
+    success: function(data) {
+        // 调用成功，得到成功的应答data
+    },
+    error: function(err) {
+        // 处理调用失败
+    }
+});
