fd6b8bc425ea646099eb282068597024fab5c19b
才龙 <hengxing0052@163.com>
2016-06-29 11:16:45 +0800

    cloud fix lessThen today

--- cloud.js
+++ cloud.js
@@ -14,11 +14,12 @@ AV.Cloud.define('hello', function(request, response) {
 
 function getRefreshTaskQuery(){
     var myDate = new Date();
+    var myDateStr = myDate.getFullYear() + '-' + (parseInt(myDate.getMonth())+1) + '-' + myDate.getDate();
 
     var query = new AV.Query(IOSAppExcLogger);
     query.equalTo('remainCount', 0);
     query.notEqualTo('taskStatus', 1);
-    query.lessThan('createdAt', myDate);
+    query.lessThan('excDateStr', myDateStr); // 今天前的日期
     query.exists('requirementImg');
     query.exists('totalExcCount');
     query.exists('excKinds');
@@ -70,14 +71,14 @@ AV.Cloud.define('refreshTask', function(request, response) {
 module.exports = AV.Cloud;
 
 
-//var paramsJson = {
-//    movie: "夏洛特烦恼"
-//};
-//AV.Cloud.run('refreshTask', paramsJson, {
-//    success: function(data) {
-//        // 调用成功，得到成功的应答data
-//    },
-//    error: function(err) {
-//        // 处理调用失败
-//    }
-//});
+var paramsJson = {
+    movie: "夏洛特烦恼"
+};
+AV.Cloud.run('refreshTask', paramsJson, {
+    success: function(data) {
+        // 调用成功，得到成功的应答data
+    },
+    error: function(err) {
+        // 处理调用失败
+    }
+});
