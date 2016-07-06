a58ed10b58e2bbc748116c1856dca36d3b0f3407
tanghui <tanghui@yangyangdeAir.lan>
2016-07-06 17:02:08 +0800

    doTask

---
+++ public/javascripts/doTask.js
@@ -0,0 +1,10 @@
+/**
+ * Created by tanghui on 16/7/6.
+ */
+
+
+var app=angular.module('yemaWebApp',[]);
+var navIndex = "";
+
+
+app.controller('doTaskCtrl', function($scope, $http, $location) {});
