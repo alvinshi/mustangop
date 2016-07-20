6b98d32323c56753951e2d58d8d8d314c320c7fb
alvinshi <alvinshi19@gmail.com>
2016-07-20 17:57:22 +0800

    taskCheck

--- public/javascripts/taskCheck.js
+++ public/javascripts/taskCheck.js
@@ -2,9 +2,36 @@ var app=angular.module('yemaWebApp',[]);
 var navIndex =3;
 
 app.controller('taskCheckCtrl', function($scope, $http, $location) {
+
+    //**************得到左侧控制器条目*******************
     var taskUrl = '/taskCheck/taskAudit';
     $http.get(taskUrl).success(function(response){
         $scope.taskAudit = response.taskAudit;
-        //$scope.taskInfo = response.taskInfo;
-    })
+        if ($scope.taskAudit.length > 0){
+            //初始显示返回的第一个
+            $scope.taskDisplayed = $scope.taskAudit[0];
+            //获取左侧信息
+            specTaskCheck($scope.taskDisplayed.taskId);
+        }
+        else {
+            //如果没有返回值, 需要在前端显示按钮
+            $scope.taskDisplayed = undefined;
+        }
+    });
+
+
+    //************点击左侧条目控制器**********************
+    $scope.check = function(app){
+        $scope.taskDisplayed = app;
+        specTaskCheck(app.taskId);
+    }
+
+    //**************  Helper Function *******************
+    var specTaskCheck = function(taskId){
+        var url = '/taskCheck/specTaskCheck/' + taskId;
+        $http.get(url).success(function(response){
+            $scope.specTask = response.rtnResults;
+            console.log($scope.specTask);
+        })
+    };
 });
--- routes/taskCheck.js
+++ routes/taskCheck.js
@@ -18,7 +18,7 @@ router.get('/', function(req, res) {
     res.render('taskCheck');
 });
 
-//
+//*************页面左侧控制器条目*************************
 router.get('/taskAudit', function(req, res){
     var userId = util.useridInReq(req);
     var user = new AV.User();
@@ -34,9 +34,12 @@ router.get('/taskAudit', function(req, res){
         for (var i = 0; i < results.length; i++){
             var appInfoObject = new Object();
             var appObject = results[i].get('appObject');
+            appInfoObject.taskId = results[i].id;
             appInfoObject.artworkUrl100 = appObject.get('artworkUrl100');
             appInfoObject.trackName = appObject.get('trackName');
-
+            appInfoObject.appId = appObject.get('appleId');
+            appInfoObject.sellerName = appObject.get('sellerName');
+            appInfoObject.latestReleaseDate = appObject.get('latestReleaseDate');
             appInfoObject.taskType = results[i].get('taskType');
             appInfoObject.excCount = results[i].get('excCount');
             appInfoObject.inProgress = results[i].get('inProgress'); // 进行中的任务
@@ -48,6 +51,46 @@ router.get('/taskAudit', function(req, res){
         }
         res.json({'taskAudit':retApps, 'taskInfo':appInfoObject})
     })
-
 });
+
+//******************点击控制器条目后触发***********************
+router.get('/specTaskCheck/:taskId', function(req, res){
+    var taskId = req.params.taskId;
+    var query = new AV.Query(receiveTaskObject);
+    console.log(taskId);
+    query.include('taskObject');
+    query.include('userObject');
+    query.ascending('createdAt');
+    query.find().then(function(results){
+        var rtnResults = new Array();
+        for (var i = 0; i < results.length; i++) {
+            var task = results[i].get('taskObject');
+            if (taskId == task.id){
+                var submission = Object();
+                var user = results[i].get('userObject')
+                submission.receiveCount = results[i].get('receiveCount');
+                submission.receivePrice = results[i].get('receivePrice');
+                submission.username = user.get('username');
+
+                //获得单个上交
+                //var relation = submission.relation('mackTask');
+                //var submission_query = relation.query();
+                //submission_query.find().then(function (data) {
+                //    submission.entries = new Array();
+                //    for (var j = 0; j < data.length; j++) {
+                //        var entry = Object();
+                //        entry.uploadName = data[j].get('uploadName');
+                //        entry.imgs = data[j].get('requirementImgs');
+                //        entry.status = data[j].get('status');
+                //        entry.detail = data[j].get('detail');
+                //        submission.entries.push(entry);
+                //    }
+                //})
+                rtnResults.push(submission);
+            }
+        }
+        console.log(rtnResults);
+        res.json({'rtnResults':rtnResults});
+    })
+})
 module.exports = router;
