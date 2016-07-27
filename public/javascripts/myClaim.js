80d2c74ae7abd2040351236a8bf04e26f4438581
tanghui <tanghui@yangyangdeAir.lan>
2016-07-27 13:11:12 +0800

    all

--- public/javascripts/myClaim.js
+++ public/javascripts/myClaim.js
@@ -8,7 +8,6 @@ var navIndex = 2;
 
 app.controller('myClaimControl', function($scope, $http, $location){
     $scope.noApp = true;
-    $scope.index=0;
 
     //$scope.hideContent=true;
     var appurlList = $location.absUrl().split('/');
@@ -18,7 +17,11 @@ app.controller('myClaimControl', function($scope, $http, $location){
 
 
     $http.get(todayUrl).success(function(response){
+        for (var i = 0; i < response.myClaimApps.length; i++){
+            response.myClaimApps[i].mode = true;
+        }
         $scope.dailyTask = response.myClaimApps;
+
         if($scope.dailyTask.length>0){
             $scope.noApp=false;
         }
@@ -28,41 +31,33 @@ app.controller('myClaimControl', function($scope, $http, $location){
     $scope.copy = $location.absUrl();
 
    //重新填写备注
-    $scope.reAssign=function(appleName){
-        for(var i=0;i<$scope.dailyTask.length;i++){
-            var tempApp = $scope.dailyTask[i];
-            if (tempApp.trackName == appleName){
-                $scope.index = i;
-                break;
+    $scope.reAssign=function(app){
+        app.mode = false;
+        console.log("changed");
 
-            }
-        }
-
-        var input=document.getElementsByClassName("assignTask")[$scope.index];
-        input.innerHTML="";
-        var input1=document.getElementsByClassName("input1")[$scope.index];
-        input1.style.display="inline-block";
+        //var input=document.getElementsByClassName("assignTask")[$scope.index];
+        //input.innerHTML="";
+        //var input1=document.getElementsByClassName("input1")[$scope.index];
+        //input1.style.display="inline-block";
         //input.value=$scope.app.detailRem;
-        var btnSave=document.getElementsByClassName("btnSave")[$scope.index];
-        btnSave.style.display="inline-block";
-        var imgpen=document.getElementsByClassName("imgpen")[$scope.index];
-        imgpen.style.display="none";
+        //var btnSave=document.getElementsByClassName("btnSave")[$scope.index];
+        //btnSave.style.display="inline-block";
+        //var imgpen=document.getElementsByClassName("imgpen")[$scope.index];
+        //imgpen.style.display="none";
 
 
 
 
     };
 
-
+    //******备注保存逻辑**********
     //保存填写的备注到数据库
 
-    $scope.saveRemark=function(detailRem, taskObjectId){
+    $scope.saveRemark=function(detailRem, taskObjectId,app){
         var saveurl = '/myClaim/saveRemark/' + userId;
         $http.post(saveurl,{"remark":detailRem, 'taskObjectId': taskObjectId}).success(
             function(response){
-                $scope.errorId = response.errorId;
-                $scope.errorMsg = response.errorMsg;
-
+                app.mode=true;
 
 
             }
