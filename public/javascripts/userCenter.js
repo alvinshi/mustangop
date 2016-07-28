4132fc952a3a6ffb42e6deb1137abd32266aac50
才龙 <hengxing0052@163.com>
2016-07-28 15:50:34 +0800

    userCenter 任务历史

--- public/javascripts/userCenter.js
+++ public/javascripts/userCenter.js
@@ -203,6 +203,17 @@ app.controller('inforManageCtrl', function($scope, $http){
     };
 });
 
+// 任务历史
+app.controller('taskHistoryCtrl', function($scope, $http){
+    var taskUrl = '/user/taskhistory';
+    $http.get(taskUrl).success(function(response){
+        $scope.ReleaseTaskHistory = response.ReleaseTaskHistory;
+        console.log($scope.ReleaseTaskHistory);
+        $scope.date = response.date;
+
+    })
+});
+
 app.controller('userCenterCtrl', function($scope, $http){
     $scope.userName = true;
 
