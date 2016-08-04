6a1da435e901d4fbd643f274e57d3247bb405857
才龙 <hengxing0052@163.com>
2016-08-05 00:19:41 +0800

    fix interiorExcDetail

--- public/javascripts/interiorExcDetail.js
+++ public/javascripts/interiorExcDetail.js
@@ -19,7 +19,7 @@ app.controller('interorDetailControl',function($scope, $http, $location){
             // 判断任务的状态
             if (response.macTask[i].status == 'uploaded' || response.macTask[i].status == 'reUploaded'){
                 response.macTask[i].status = '待审'
-            }else if (response.macTask[i].status == 'rejected'){
+            }else if (response.macTask[i].status == 'rejected' || response.macTask[i].status == "refused"){
                 response.macTask[i].status = '拒绝'
             }else if (response.macTask[i].status == 'accepted' || response.macTask[i].status == 'systemAccepted'){
                 response.macTask[i].status = '已完成'
