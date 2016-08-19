79228d6d76ad59dc8eec75e874d13a2a44771119
tanghui <tanghui@yangyangdeAir.lan>
2016-08-19 11:30:15 +0800

    支付Y币

--- public/javascripts/userCenter.js
+++ public/javascripts/userCenter.js
@@ -17,6 +17,11 @@ app.config(['$stateProvider','$urlRouterProvider',function($stateProvider, $urlR
             templateUrl:'/html/userCenter-account.html',
             controller:'userCenterCtrl'
         })
+        .state('YRecharge',{
+            url:'/YRecharge',
+            templateUrl:'/html/userCenter-YRecharge.html',
+            controller:'YRecharge'
+        })
         .state('inforManage',{
             url:'/inforManage',
             templateUrl:'/html/userCenter-inforManage.html',
