a3545be1e55f26ce6d602acab3d7e6b03953f25d
tanghui <tanghui@yangyangdeAir.lan>
2016-08-29 17:12:13 +0800

    ...

--- public/javascripts/taskCheck.js
+++ public/javascripts/taskCheck.js
@@ -26,7 +26,7 @@ app.controller('taskCheckCtrl', function($scope, $http, $location) {
 
         if ($scope.taskAudit.length > 0){
             //初始显示返回的第一个
-            $scope.taskAudit = response.taskAudit.sort(function(b, a){return a.totalSubmited >= b.totalSubmited});
+            $scope.taskAudit = response.taskAudit.sort(function compare(b,a ){return a.totalSubmited - b.totalSubmited});
             $scope.taskIndex = 0;
             $scope.taskDisplayed = $scope.taskAudit[0];
         }
