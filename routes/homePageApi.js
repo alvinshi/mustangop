ae5225a9440aabea2ce164fbc396360e9953e664
wujiangwei55 <wujiangwei55@gmail.com>
2016-09-07 14:34:57 +0800

    Fix check in bug

--- routes/homePageApi.js
+++ routes/homePageApi.js
@@ -95,7 +95,7 @@ router.get('/ischeckins', function(req, res){
                 res.json({'errorId':0, 'isCheckIns': 0, 'todayYB': tomorrowGiftYb, 'latestDays':latestDays, 'continueCheck': (tomorrowGiftYb+1>maxCheckIn) ? maxCheckIn : tomorrowGiftYb+1})
             }
             else {
-                res.json({'errorId':0, 'isCheckIns': 0, 'todayYB': 1, 'latestDays':latestDays,  'continueCheck': 2})
+                res.json({'errorId':0, 'isCheckIns': 0, 'todayYB': 1, 'latestDays':0,  'continueCheck': 2})
             }
         }
     },function(error){
