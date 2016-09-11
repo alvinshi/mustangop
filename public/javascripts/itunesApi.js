a41888f042cf4b868537e0a9dadb001427d63c3e
才龙 <hengxing0052@163.com>
2016-09-11 16:58:26 +0800

    myApp HTML

--- public/javascripts/itunesApi.js
+++ public/javascripts/itunesApi.js
@@ -557,4 +557,33 @@ app.controller('itunesSearchControl', function($scope, $http) {
         $scope.saved = false;
     };
 
+    $.fn.smartFloat = function() {
+        var position = function(element) {
+            var top = element.position().top, pos = element.css("position");
+            $(window).scroll(function() {
+                var scrolls = $(this).scrollTop();
+                if (scrolls > top) {
+                    if (window.XMLHttpRequest) {
+                        element.css({
+                            position: "fixed",
+                            top: 0
+                        });
+                    } else {
+                        element.css({
+                            top: scrolls
+                        });
+                    }
+                }else {
+                    element.css({
+                        position: "relative",
+                        top: 0
+                    });
+                }
+            });
+        };
+        return $(this).each(function() {
+            position($(this));
+        });
+    };
+    $("#float").smartFloat();
 });
